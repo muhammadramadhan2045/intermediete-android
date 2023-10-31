86fd0b601b5354c2b915fa34bacd679b6dcbaa46
Muhammad Ramadhan <muhammadramadhan2045@gmail.com>
2023-10-31 16:55:37 +0700

    mengubah rotasi file piksel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,6 +51,8 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1") //untuk lifecycleScope
 
+    implementation("androidx.exifinterface:exifinterface:1.3.6")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
+++ app/src/main/java/com/dicoding/picodiploma/mycamera/Utils.kt
@@ -4,6 +4,8 @@ import android.content.ContentValues
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.Matrix
+import androidx.exifinterface.media.ExifInterface
 import android.net.Uri
 import android.os.Build
 import android.os.Environment
@@ -71,13 +73,13 @@ fun uriToFile(imageUri: Uri, context: Context): File {
 
 fun File.reduceFileImage(): File {
     val file = this
-    val bitmap = BitmapFactory.decodeFile(file.path)
+    val bitmap = BitmapFactory.decodeFile(file.path).getRotatedBitmap(file)
     var compressQuality = 100
     var streamLength: Int
 
     do {
         val bmpStream = ByteArrayOutputStream()
-        bitmap.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpStream)
+        bitmap?.compress(Bitmap.CompressFormat.JPEG, compressQuality, bmpStream)
         val bmpPicByteArray = bmpStream.toByteArray()
         streamLength = bmpPicByteArray.size
         compressQuality -= 5
@@ -86,6 +88,28 @@ fun File.reduceFileImage(): File {
     bitmap?.compress(Bitmap.CompressFormat.JPEG, compressQuality, FileOutputStream(file))
     return file
 }
+
+fun Bitmap.getRotatedBitmap(file: File):Bitmap?{
+    val orientation=ExifInterface(file).getAttributeInt(
+        ExifInterface.TAG_ORIENTATION,ExifInterface.ORIENTATION_UNDEFINED
+    )
+    return when(orientation){
+        ExifInterface.ORIENTATION_ROTATE_90 -> rotateImage(this, 90F)
+        ExifInterface.ORIENTATION_ROTATE_180 -> rotateImage(this, 180F)
+        ExifInterface.ORIENTATION_ROTATE_270 -> rotateImage(this, 270F)
+        ExifInterface.ORIENTATION_NORMAL -> this
+        else -> this
+    }
+}
+
+fun rotateImage(source: Bitmap, angle: Float): Bitmap? {
+    val matrix=Matrix()
+    matrix.postRotate(angle)
+    return Bitmap.createBitmap(
+        source,0,0,source.width,source.height,matrix,true
+    )
+}
+
 
 class Utils {
 
