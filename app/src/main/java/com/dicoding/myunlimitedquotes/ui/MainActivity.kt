e6bca0b6771cca9377dcf9658b1c45d0625d1306
Muhammad Ramadhan <muhammadramadhan2045@gmail.com>
2023-11-05 10:15:41 +0700

    menambahkan loading

--- app/src/main/java/com/dicoding/myunlimitedquotes/ui/MainActivity.kt
+++ app/src/main/java/com/dicoding/myunlimitedquotes/ui/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.dicoding.myunlimitedquotes.adapter.LoadingStateAdapter
 import com.dicoding.myunlimitedquotes.adapter.QuoteListAdapter
 import com.dicoding.myunlimitedquotes.databinding.ActivityMainBinding
 
@@ -26,7 +27,11 @@ class MainActivity : AppCompatActivity() {
 
     private fun getData() {
         val adapter = QuoteListAdapter()
-        binding.rvQuote.adapter = adapter
+        binding.rvQuote.adapter = adapter.withLoadStateFooter(
+            footer=LoadingStateAdapter{
+                adapter.retry()
+            }
+        )
         mainViewModel.quote.observe(this, {
             adapter.submitData(lifecycle, it)
         })
